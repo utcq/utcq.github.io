7d0f5ecf736bfff959a059e28f22ed55a54b380c
Un!ty <108679939+utcq@users.noreply.github.com>
2024-06-03 18:41:18 +0200

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -25,7 +25,7 @@ const CONFIG = {
       },
       manual: {
         // Properties for manually specifying projects
-        projects: ['utcq/soel', 'funixorg/Saturn9', 'utcq/rave', 'utcq/olicybersec', 'utcq/oca', 'utcq/AseV'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+        projects: ['utcq/soel', 'funixorg/Saturn9', 'utcq/rave', 'utcq/olicybsec', 'utcq/oca', 'utcq/AseV'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
       },
     },
     external: {
