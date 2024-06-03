721d755ba5f31f3be0ada0b2e35f6d251f434b23
Un!ty <108679939+utcq@users.noreply.github.com>
2024-06-03 18:31:48 +0200

    upd

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -154,7 +154,7 @@ const CONFIG = {
   // Display articles from your medium or dev account. (Optional)
   blog: {
     source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
+    username: '', // to hide blog section, keep it empty
     limit: 2, // How many articles to display. Max is 10.
   },
   googleAnalytics: {
